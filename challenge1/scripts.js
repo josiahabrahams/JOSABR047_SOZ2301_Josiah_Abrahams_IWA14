0b3ea8cf4f502193dd2f6e93aa1e87862225a7bc
josiahabrahams <josiahabrahams1@gmail.com>
2023-06-08 13:52:33 +0200

    added js doc

--- challenge1/scripts.js
+++ challenge1/scripts.js
@@ -1,6 +1,13 @@
+//@ts-check
 const firstName = 'John';
 const age = 35;
 const myHobby = 'Coding';
+
+/**
+ *  this funcition will console log parameter aFunction twice
+ * @param {*} aFunction - will be repeated twice by function 
+ * @returns {void}
+ */
 
 const logTwice = (aFunction)=> {
     
@@ -8,9 +15,12 @@ const logTwice = (aFunction)=> {
   console.log(aFunction)
   console.log(aFunction)
 }
-
+ /**
+  * this function produces a string using the info from above
+  * @returns {string}
+  */
 function hobby () {
-  return`Hello, ${ firstName} (${age}). I love ${myHobby}!`
+  return `Hello, ${firstName} (${age}). I love ${myHobby}!`
 }
 
 logTwice(hobby())
